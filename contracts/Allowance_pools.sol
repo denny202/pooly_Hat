635370586794422dd07027b3772d7870952b4c97
denny202 <iomassimetto@gmail.com>
2021-11-13 22:48:56 +0000

    Events added modified:   Allowance_pools.sol

--- contracts/Allowance_pools.sol
+++ contracts/Allowance_pools.sol
@@ -6,10 +6,18 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract WalletAllowance is Ownable{
+contract Wallet_allowance is Ownable{
+
     
+    mapping(address=>uint) public allowance_map;
+
+
+    event Allowance_event(address indexed _toperson , address indexed _fromperson,uint _oldamount,uint _newamount );
+
+    event sentFunds(address indexed _to,uint _amount );
     
-    mapping(address=>uint) public allowanceMap;
+    event depositedFunds(address indexed _from ,uint _amount);
+
     
     function isOwner()public view returns(bool){
         return msg.sender==owner();
@@ -17,20 +25,23 @@ contract WalletAllowance is Ownable{
     
     
     modifier owner_and_managers(uint _amount){
-        require ( isOwner() || allowanceMap[msg.sender] >= _amount ,"Not enough allowance");
+        require ( isOwner() || allowance_map[msg.sender] >= _amount ,"Not enough allowance");
         _;
     }
     
     
     function allow_account(address _person,uint _amount)public onlyOwner{
-        allowanceMap[_person]=_amount;
+        emit Allowance_event(_person,msg.sender,allowance_map[_person],_amount);
+        
+        allowance_map[_person]=_amount;
     }
 
     //reduce allowance when withdrawMoney
     function reduce_allowance(address  _person,uint _amount ) internal{
         
+        emit Allowance_event(_person,msg.sender,allowance_map[_person],allowance_map[_person]-_amount);
         //set allowance
-        allowanceMap[_person]-=_amount;
+        allowance_map[_person]-=_amount;
         
     }
     
@@ -41,26 +52,23 @@ contract WalletAllowance is Ownable{
         
     }
     
-    //
+
     function withdraw_Money(address payable _to ,uint _amount)public owner_and_managers(_amount) {
         require(_amount <= address(this).balance,"There are no enough Wei's")    ; 
          if (!isOwner()){
              reduce_allowance(msg.sender,_amount);
          }
-         
+         emit sentFunds(_to,_amount);
         _to.transfer(_amount);
      }
     
     //deposi
     function deposit ()external payable  {
         
+        emit depositedFunds(msg.sender,msg.value);
         
     }
     
-    
-    
-    
-    
-    
+
     
 } 
